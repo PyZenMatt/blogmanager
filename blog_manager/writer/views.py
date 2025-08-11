31aec9626f82855aff7315b9599f53e9b36b6316
PyZenMatt <matteoricci12@outlook.it>
2025-08-11 14:24:03 +0200

    All flake8 linting issues in views.py have been resolved. The file now conforms to Python style guidelines.

--- blog_manager/writer/views.py
+++ blog_manager/writer/views.py
@@ -1,9 +1,9 @@
-from django.contrib import messages
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
+
 
 class BootstrapLoginView(auth_views.LoginView):
     template_name = "writer/login.html"
@@ -17,18 +17,23 @@ class BootstrapLoginView(auth_views.LoginView):
             classes = field.widget.attrs.get("class", "")
             field.widget.attrs["class"] = (classes + " form-control").strip()
             field.widget.attrs.setdefault("autocomplete", name)
-        form.fields["username"].widget.attrs.setdefault("placeholder", "Username")
-        form.fields["password"].widget.attrs.setdefault("placeholder", "Password")
+        form.fields["username"].widget.attrs.setdefault(
+            "placeholder", "Username"
+        )
+        form.fields["password"].widget.attrs.setdefault(
+            "placeholder", "Password"
+        )
         return form
 
     def form_valid(self, form):
         # Handle "remember me": if unchecked, session expires on browser close
         remember = self.request.POST.get("remember") == "on"
-        response = super().form_valid(form)
+        super().form_valid(form)
         if not remember:
             self.request.session.set_expiry(0)  # expire at browser close
         # Redirect to post list after login
         return redirect("writer:post_list")
+
 
 class BootstrapLogoutView(auth_views.LogoutView):
     next_page = reverse_lazy("writer:login")
@@ -38,24 +43,22 @@ class BootstrapLogoutView(auth_views.LogoutView):
         # Add flag to show "logged out" banner in login
         return f"{base}?logged_out=1" if base else None
 
+
 @login_required
 def post_new(request):
     return render(request, "writer/post_new.html")
 
 
-  
 @login_required
 def post_edit(request, id):
     return render(request, "writer/post_edit.html", {"post_id": id})
 
 
-  
 @login_required
 def taxonomy(request):
-    
     return render(request, "writer/taxonomy.html")
 
-  
+
 @login_required
 def post_list(request):
     return render(request, "writer/posts_list.html")
