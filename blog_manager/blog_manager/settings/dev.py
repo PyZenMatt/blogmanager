bf521aedacc851eef68cde05978f55c4b4c79461
PyZenMatt <matteoricci12@outlook.it>
2025-08-15 12:00:53 +0200

    Disabilita export git in produzione e sviluppo tramite EXPORT_ENABLED

--- blog_manager/blog_manager/settings/dev.py
+++ blog_manager/blog_manager/settings/dev.py
@@ -1,6 +1,7 @@
 from .base import *
 
 DEBUG = True
+EXPORT_ENABLED = False
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 CORS_ALLOWED_ORIGINS = env.list(
     "CORS_ALLOWED_ORIGINS", default=["http://localhost:8000"]
